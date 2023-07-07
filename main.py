b09bf2344a0e1fc24e5498df0e7be8265fe3dbae
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2023-07-08 02:20:34 +0800

    Added documentation comments

--- main.py
+++ main.py
@@ -26,43 +26,48 @@ def returnCameraIndices():
         i -= 1
     return arr
 
-
-# cap = cv2.VideoCapture(returnCameraIndices[-1])
+# 1.1 cap = cv2.VideoCapture(returnCameraIndices[-1])
 url = "http://192.168.1.39:8080/video"
 cap = cv2.VideoCapture(url)
-
-# Камера может снимать в высоком разрешении, поэтому его лучше предварительно уменьшить
-resize_scale = 2
 
 if (cap.isOpened()== False): 
     print("Error opening video stream or file")
 
+# 1.2
+# Камера может снимать в высоком разрешении, поэтому его лучше предварительно уменьшить
+resize_scale = 2
 board_present = False
 my_board = Board()
 borders = tuple()
 
+# 1.3
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        frame = cv2.resize(frame, (frame.shape[1] // resize_scale, frame.shape[0] // resize_scale))
+        frame = cv2.resize(frame, (frame.shape[1] // resize_scale, frame.shape[0] // resize_scale)) # 1.4
+        
+        # 1.5
         if cv2.waitKey(1) & 0xFF == ord('b'):
             b_present = my_board.find_board(frame, borders)
             if not b_present:
                 print('No board detected!')
                 continue
-            my_board.rotate(frame, True)
-            my_board.find_board(frame, borders)
+            
+            my_board.rotate(frame, True) # 1.6
+            my_board.find_board(frame, borders) # 1.7
             board_present = True
 
         if board_present:
-            my_board.rotate(frame)
-            my_board.check_lights(frame)
-            my_board.draw(frame)
+            my_board.rotate(frame) # 1.8
+            my_board.check_lights(frame) # 1.9
+            my_board.draw(frame) # 1.10
             
         else:
-            borders = draw_borders(frame, 20)
+            borders = draw_borders(frame, 20) # 1.11
 
-        cv2.imshow('Frame', frame)         
+        cv2.imshow('Frame', frame) # 1.12
+        
+        # 1.13
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else: 
